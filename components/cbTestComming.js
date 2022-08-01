8cdad71e8d80fc9a715f476a6b8f93e673f071bc
ilnazway <localhost@mail.loc>
2022-08-01 23:25:19 +0300

    Тестирование возможностей vue

--- components/cbTestComming.js
+++ components/cbTestComming.js
@@ -5,8 +5,43 @@ const template = {
       console.log('mountend');
     });
 
-    return {};
+    const msg = Vue.ref('Тестовое сообщение!');
+    const check = Vue.ref(false)
+
+    return { msg, check };
   },
+  data() {
+    return {
+      classObj: {
+        display: 'grid',
+      }
+    }
+  },
+  methods: {
+    turnCheck() {
+      const condition = !this.check;
+      this.check = condition;
+    }
+  },
+  template: /*html*/`
+    <h2 :title="msg">{{ msg.toLocaleUpperCase() }}</h2>
+    <input v-model="msg">
+    <p v-if="check">Many text</p>
+    <input :style="classObj" :class="{ classObj: !check }" type="checkbox" :checked="check" @click="turnCheck()">
+  `,
+  styles: /*css*/`
+    .di-block {
+      display: block;
+    }
+  `,
 }
 
-const cbTestComming = Vue.createApp()
+const cbTestComming = Vue.createApp(template);
+cbTestComming.mount('#cb-comming');
+
+const app = Vue.createApp({})
+
+app.component('my-component', {
+  template: '<p class="foo bar">Привет</p>'
+});
+app.mount('#cb-comming3');
